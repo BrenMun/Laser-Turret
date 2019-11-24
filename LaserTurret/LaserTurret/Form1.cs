1ce7d9724f9ddfe8a642dd8d3179a3f816964929
BrenMun <49784557+BrenMun@users.noreply.github.com>
2019-11-25 02:30:18 +1100

    Update Form1.cs

--- LaserTurret/LaserTurret/Form1.cs
+++ LaserTurret/LaserTurret/Form1.cs
@@ -12,23 +12,30 @@ namespace LaserTurret
         {
             InitializeComponent();
         }
-
+        
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             watch = Stopwatch.StartNew();
-            port.Open();
+            port.Open(); //open serial port to communicate with the arduino
         }
-
+        
+        //send serial data when the mouse moves on form1
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            writeToPort(new Point(e.X, e.Y));
+            writeToPort(new Point(e.X, e.Y));   //pass event data
         }
-            public void writeToPort(Point coordinates)
+            public void writeToPort(Point coordinates) //set up write data for arduino
             {
-            if (watch.ElapsedMilliseconds > 15)
+            if (watch.ElapsedMilliseconds > 15)     //limits amount of serial data being sent, relsoving latencey issue 
             {
                 watch = Stopwatch.StartNew();
-                port.Write(String.Format("X{0}Y{1}", (180 -coordinates.X / (Size.Width / 180)), (180 - coordinates.Y / (Size.Height / 180))));
+                port.Write(String.Format("X{0}Y{1}",            //format xy coordinates using string
+                (180 -coordinates.X / (Size.Width / 180)),      //scales data X: divides the X coordinate by the width of the form over the angle of rotation of the servo 
+                (180 - coordinates.Y / (Size.Height / 180))));  //data Y: divides Y coordinate by the height of the form over the angle of rotation of the servo
+                
+                //due to the position of the servos, the turret moved opposite to the movement of the mouse
+                //Therefore, I added "180 -" to invert the direction resolving the issue
             }
              }
        
